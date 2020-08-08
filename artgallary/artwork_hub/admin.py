c2ed52a897b0fc3e08b41c11458320c8328621cf
xie186 <xie186@purdue.edu>
2020-08-08 11:06:22 -0400

    Update code

---
+++ artgallary/artwork_hub/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from artwork_hub.models import Artwork,Pic
+# Register your models here.
+admin.site.register(Artwork)
+admin.site.register(Pic)
+
+
---
+++ artgallary/artwork_hub/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ArtworkHubConfig(AppConfig):
+    name = 'artwork_hub'
